83ef8e8a8624d6791d3bc1b6e4e00ea6ea01a23c
= <tiagospem@gmail.com>
2025-02-03 13:46:08 -0300

    wip table

---
+++ resources/views/layout/table.blade.php
@@ -0,0 +1,62 @@
+<div class="px-4 sm:px-6 lg:px-8">
+    @if(!isset($data['columns']))
+        <p>Please define the columns</p>
+    @else
+        <input wire:model.live.debounce="search" />
+
+        <div class="mt-8 flow-root">
+            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <div class="overflow-hidden shadow ring-1 ring-black/5 sm:rounded-lg">
+                        <x-simple-tables::table>
+                            <x-simple-tables::thead>
+                                <x-simple-tables::tr>
+                                    @foreach($data['columns'] as $column)
+                                        <x-simple-tables::th>
+                                            {{ $column['title'] }}
+                                        </x-simple-tables::th>
+
+                                        @if($loop->last)
+                                            <x-simple-tables::th class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                                <span class="sr-only">action</span>
+                                            </x-simple-tables::th>
+                                        @endif
+                                    @endforeach
+                                </x-simple-tables::tr>
+                            </x-simple-tables::thead>
+                            <x-simple-tables::tbody>
+                                @forelse($data['rows'] as $row)
+                                    <x-simple-tables::tr>
+                                        @foreach($data['columns'] as $column)
+                                            <x-simple-tables::td>
+                                                {{ data_get($row, $column['field']) }}
+                                            </x-simple-tables::td>
+
+                                            @if($loop->last)
+                                                <x-simple-tables::td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">action</a>
+                                                </x-simple-tables::td>
+                                            @endif
+                                        @endforeach
+                                    </x-simple-tables::tr>
+                                @empty
+                                    <x-simple-tables::tr>
+                                        <x-simple-tables::td colspan="9999">
+                                            <div class="text-center">
+                                                <p class="text-sm text-gray-500">
+                                                    {{ __('simple-tables::table.no-records') }}
+                                                </p>
+                                            </div>
+                                        </x-simple-tables::td>
+                                    </x-simple-tables::tr>
+                                @endforelse
+                            </x-simple-tables::tbody>
+                        </x-simple-tables::table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{ $data['rows']->links() }}
+    @endif
+</div>
---
+++ src/Concerns/TableStyle.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TiagoSpem\SimpleTables\Concerns;
+
+trait TableStyle
+{
+    // todo implement a way to replace table style, set default styles of table, td, th, etc and replace here for individual tables
+}
