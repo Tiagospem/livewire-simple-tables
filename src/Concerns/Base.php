8b9b4ce4096fb33bdbdd608273ef71745beeaa76
= <tiagospem@gmail.com>
2025-02-03 13:45:36 -0300

    added base

---
+++ src/Concerns/Base.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace TiagoSpem\SimpleTables\Concerns;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Livewire\WithPagination;
+use TiagoSpem\SimpleTables\Datasource\Processor;
+use TiagoSpem\SimpleTables\Exceptions\InvalidColumnException;
+
+trait Base
+{
+    use WithPagination;
+
+    public ?string $search = '';
+
+    public string $sortBy = 'id';
+
+    public string $sortDirection = 'desc';
+
+    public bool $paginated = true;
+
+    public int $perPage = 15;
+
+    abstract public function columns(): array;
+
+    abstract public function datasource(): Builder|Collection;
+
+    /**
+     * @throws InvalidColumnException
+     */
+    protected function getData(): ?array
+    {
+        return (new Processor($this))->process();
+    }
+}
