ff0eb1b3aa5b7cde1642a3245e636bde3802fedd
= <tiagospem@gmail.com>
2025-02-03 13:43:13 -0300

    added command to generate tables

--- src/Commands/CreateCommand.php
+++ src/Commands/CreateCommand.php
@@ -7,13 +7,56 @@ use Illuminate\Console\Command;
 class CreateCommand extends Command
 {
     /** @var string */
-    protected $signature = 'simple-table:create {--template= : name of the file that will be used as a template}';
+    protected $signature = 'simple-table:create {name : The name of the SimpleTable component}';
 
     /** @var string */
     protected $description = 'Make a new SimpleTable component.';
 
     public function handle(): int
     {
+        $name = $this->argument('name');
+
+        $stubPath = __DIR__.'/../../resources/stubs/table.stub';
+
+        if (! file_exists($stubPath)) {
+            $this->error('stub not found');
+
+            return self::FAILURE;
+        }
+
+        $name = str_replace(['\\', '/'], '/', $name);
+        $parts = explode('/', $name);
+        $className = array_pop($parts);
+        $subPath = ! empty($parts) ? implode('/', $parts).'/' : '';
+
+        $basePath = config('simple-tables.create-path');
+        $targetPath = $basePath.'/'.$subPath.$className.'.php';
+
+        if (file_exists($targetPath)) {
+            $this->error('Component already exists: '.$targetPath);
+
+            return self::FAILURE;
+        }
+
+        if (! is_dir(dirname($targetPath))) {
+            mkdir(dirname($targetPath), 0755, true);
+        }
+
+        $relativePath = ltrim(str_replace(app_path(), '', $basePath), '\\/');
+        $namespaceBase = 'App\\'.str_replace('/', '\\', $relativePath);
+        $namespace = $namespaceBase.($parts ? '\\'.implode('\\', $parts) : '');
+
+        $content = file_get_contents($stubPath);
+        $content = str_replace(
+            ['{{ namespace }}', '{{ class }}'],
+            [$namespace, $className],
+            $content
+        );
+
+        file_put_contents($targetPath, $content);
+
+        $this->info('Component created: '.$targetPath);
+
         return self::SUCCESS;
     }
 }
