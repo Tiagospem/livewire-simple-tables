8fdbe20475fa363e8afa35522b2e6b9b7e2c3b3a
= <tiagospem@gmail.com>
2025-02-03 13:43:29 -0300

    added column class

---
+++ src/Column.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace TiagoSpem\SimpleTables;
+
+use Livewire\Wireable;
+
+final class Column implements Wireable
+{
+    public string $title = '';
+
+    public string $field = '';
+
+    public bool $searchable = true;
+
+    public static function add(string $title, string $field, bool $searchable = true): self
+    {
+        $column = new self;
+        $column->title = $title;
+        $column->field = $field;
+        $column->searchable = $searchable;
+
+        return $column;
+    }
+
+    public function notSearchable(): self
+    {
+        $this->searchable = false;
+
+        return $this;
+    }
+
+    public function getField(): string
+    {
+        return $this->field;
+    }
+
+    public function toLivewire(): array
+    {
+        return (array) $this;
+    }
+
+    public static function fromLivewire($value)
+    {
+        return $value;
+    }
+}
