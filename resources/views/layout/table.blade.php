660f19fff58f76c697bf762f5e7a3f081258ca5a
Tiago Padilha <tiagospem@gmail.com>
2025-02-06 14:53:56 -0300

    refactor view

--- resources/views/layout/table.blade.php
+++ resources/views/layout/table.blade.php
@@ -1,5 +1,5 @@
 <div class="px-4 sm:px-6 lg:px-8">
-    @if(!isset($data['columns']))
+    @if (!isset($data['columns']))
         <p>Please define the columns</p>
     @else
         <div class="flex justify-end mt-4">
@@ -12,41 +12,58 @@
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                     <div class="overflow-hidden shadow ring-1 ring-black/5 sm:rounded-lg">
-                        <x-simple-tables::table :class="theme($theme, 'table.content')">
-                            <x-simple-tables::thead :class="theme($theme, 'table.thead')">
-                                <x-simple-tables::tr :class="theme($theme, 'table.tr')">
+                        @php
+                            $tableClass = theme($theme, 'table.content');
+                            $theadClass = theme($theme, 'table.thead');
+                            $tbodyClass = theme($theme, 'table.tbody');
+                            $trClass = theme($theme, 'table.tr');
+                            $tdClass = theme($theme, 'table.td');
+                            $thClass = theme($theme, 'table.th');
+                            $thLastClass = theme($theme, 'table.th_last');
+                            $tdNoRecordsClass = theme($theme, 'table.td_no_records');
+                        @endphp
+
+                        <x-simple-tables::table :class="$tableClass">
+                            <x-simple-tables::thead :class="$theadClass">
+                                <x-simple-tables::tr :class="$trClass">
                                     @foreach($data['columns'] as $column)
-                                        <x-simple-tables::th :class="theme($theme, 'table.th')">
+                                        <x-simple-tables::th :class="$thClass">
                                             {{ $column['title'] }}
                                         </x-simple-tables::th>
 
-                                        @if($loop->last)
-                                            <x-simple-tables::th :class="theme($theme, 'table.th_last')">
+                                        @if ($loop->last)
+                                            <x-simple-tables::th :class="$thLastClass">
                                                 <span class="sr-only">action</span>
                                             </x-simple-tables::th>
                                         @endif
                                     @endforeach
                                 </x-simple-tables::tr>
                             </x-simple-tables::thead>
-                            <x-simple-tables::tbody :class="theme($theme, 'table.tbody')">
+
+                            <x-simple-tables::tbody :class="$tbodyClass">
                                 @php
                                     $modifiers = $data['modifiers'];
                                     $styleModifier = $data['styleModifier'];
                                 @endphp
-                                @forelse($data['rows'] as $row)
+
+                                @forelse ($data['rows'] as $row)
                                     @php
-                                        $style = style($styleModifier, $row)
+                                        $style = style($styleModifier, $row);
+                                        $dynamicTrClass = $style['tr'] ?? $trClass;
                                     @endphp
-                                    <x-simple-tables::tr :class="theme($theme, 'table.tr', $style['tr'])">
+
+                                    <x-simple-tables::tr :class="$dynamicTrClass">
                                         @foreach($data['columns'] as $column)
                                             @php
-                                                $parsedData = parseData($modifiers, $column, $row)
+                                                $parsedData = parseData($modifiers, $column, $row);
+                                                $dynamicTdClass = $style['td'] ?? $parsedData['tdStyle'] ?? $tdClass;
                                             @endphp
-                                            <x-simple-tables::td :class="theme($theme, 'table.td', $style['td'] ?? $parsedData['tdStyle'])">
-                                                {!! e($parsedData['content']) !!}
+
+                                            <x-simple-tables::td :class="$dynamicTdClass">
+                                                {{ $parsedData['content'] }}
                                             </x-simple-tables::td>
 
-                                            @if($loop->last)
+                                            @if ($loop->last)
                                                 <x-simple-tables::td :class="theme($theme, 'table.td_last')">
                                                     <a href="#" class="text-indigo-600 hover:text-indigo-900">action</a>
                                                 </x-simple-tables::td>
@@ -54,8 +71,8 @@
                                         @endforeach
                                     </x-simple-tables::tr>
                                 @empty
-                                    <x-simple-tables::tr :class="theme($theme, 'table.tr')">
-                                        <x-simple-tables::td colspan="9999" :class="theme($theme, 'table.td_no_records')">
+                                    <x-simple-tables::tr :class="$trClass">
+                                        <x-simple-tables::td colspan="9999" :class="$tdNoRecordsClass">
                                             {{ __('simple-tables::table.no-records') }}
                                         </x-simple-tables::td>
                                     </x-simple-tables::tr>
