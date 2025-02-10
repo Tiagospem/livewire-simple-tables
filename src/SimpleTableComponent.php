6d1139eb7ef7ab7e20b73e4d2d26e18a6551b342
Tiago Padilha <tiagospem@gmail.com>
2025-02-10 18:07:51 -0300

    refactor search query

--- src/SimpleTableComponent.php
+++ src/SimpleTableComponent.php
@@ -3,19 +3,57 @@
 namespace TiagoSpem\SimpleTables;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Livewire\Component;
+use Livewire\WithPagination;
 use TiagoSpem\SimpleTables\Concerns\ActionBuilder;
-use TiagoSpem\SimpleTables\Concerns\Base;
 use TiagoSpem\SimpleTables\Concerns\DataModifier;
 use TiagoSpem\SimpleTables\Concerns\SearchModifier;
 use TiagoSpem\SimpleTables\Concerns\StyleModifier;
 use TiagoSpem\SimpleTables\Concerns\ThemeManager;
+use TiagoSpem\SimpleTables\Datasource\Processor;
 use TiagoSpem\SimpleTables\Exceptions\InvalidColumnException;
 use TiagoSpem\SimpleTables\Exceptions\InvalidParametersException;
 
 abstract class SimpleTableComponent extends Component
 {
-    use ActionBuilder, Base, DataModifier, SearchModifier, StyleModifier, ThemeManager;
+    use ActionBuilder, DataModifier, SearchModifier, StyleModifier, ThemeManager, WithPagination;
+
+    private bool $showSearch = false;
+
+    public ?string $search = '';
+
+    public string $sortBy = 'id';
+
+    public string $sortDirection = 'desc';
+
+    public bool $paginated = true;
+
+    public int $perPage = 10;
+
+    public function mount(): void
+    {
+        $this->showSearch = collect($this->columns())
+            ->filter(fn (Column $column): bool => $column->searchable)
+            ->isNotEmpty();
+    }
+
+    public function updatedSearch(): void
+    {
+        $this->resetPage();
+    }
+
+    /**
+     * @return array<int, Column>
+     */
+    abstract public function columns(): array;
+
+    /**
+     * @return Builder<Model>|Collection<int, mixed>
+     */
+    abstract public function datasource(): Builder|Collection;
 
     /**
      * @throws InvalidColumnException
@@ -24,8 +62,9 @@ abstract class SimpleTableComponent extends Component
     public function render(): View
     {
         return view('simple-tables::layout.table', [
-            'data' => $this->getData(),
+            'data' => (new Processor($this))->process(),
             'theme' => $this->theme,
+            'showSearch' => $this->showSearch,
         ]);
     }
 }
