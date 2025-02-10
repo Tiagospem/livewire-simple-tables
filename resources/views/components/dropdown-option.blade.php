10ad78f0cf66efb324dd39937b0065e77b05768a
Tiago Padilha <tiagospem@gmail.com>
2025-02-09 21:41:00 -0300

    new build

--- resources/views/components/dropdown-option.blade.php
+++ resources/views/components/dropdown-option.blade.php
@@ -1,4 +1,11 @@
-@props(['title' => '', 'icon' => '', 'disabled' => false, 'clickEvent' => [], 'iconStyle' => '', 'buttonStyle' => ''])
+@props([
+    'title' => '',
+    'icon' => '',
+    'disabled' => false,
+    'clickEvent' => [],
+    'iconStyle' => '',
+    'buttonStyle' => '',
+])
 <div
     @class(['py-1', 'opacity-50 pointer-events-none' => $disabled])
     role="none"
@@ -6,22 +13,17 @@
 >
     <a
         x-on:click="handleClick({{ json_encode($clickEvent) }})"
-        class="{{
-          mergeClass(
-          'hover:bg-gray-100 group flex items-center px-4 py-2 text-sm text-gray-700 cursor-pointer outline-none focus:outline-none',
-            $buttonStyle
-          )
-        }}"
+        class="{{ mergeClass(
+            'hover:bg-gray-100 group flex items-center px-4 py-2 text-sm text-gray-700 cursor-pointer outline-none focus:outline-none',
+            $buttonStyle,
+        ) }}"
         role="menuitem"
         tabindex="-1"
     >
         @if (filled($icon))
             <x-dynamic-component
                 :component="$icon"
-                @class([
-                    'mr-3 size-5 text-gray-400',
-                    $iconStyle,
-                ])
+                @class(['mr-3 size-5 text-gray-400', $iconStyle])
             />
         @endif
 
