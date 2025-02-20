fdc3d1713a41ea4930622c067b672df7d97e0cf8
Tiago Padilha <tiagospem@gmail.com>
2025-02-20 10:43:45 -0300

    added unit test for helper functions

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,3 +3,10 @@
 use TiagoSpem\SimpleTables\Tests\TestCase;
 
 uses(TestCase::class)->in('Feature', 'Unit');
+
+if (! function_exists('mb_trim')) {
+    function mb_trim(string $str): string
+    {
+        return trim($str);
+    }
+}
