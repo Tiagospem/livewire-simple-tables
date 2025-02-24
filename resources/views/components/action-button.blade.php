adcbdf9132054508fb09758d2a00f67ff072ac98
Tiago Padilha <tiagospem@gmail.com>
2025-02-23 21:12:40 -0300

    feat: enhance action button functionality with URL and event handling; add ActionBuilderTest for validation

--- resources/views/components/action-button.blade.php
+++ resources/views/components/action-button.blade.php
@@ -1,70 +1,70 @@
-@props(['actionBuilder', 'themeButtonActionClass', 'themeDropdownOptionClass', 'themeDropdownClass', 'row'])
+{{--@props(['actionBuilder', 'themeButtonActionClass', 'themeDropdownOptionClass', 'themeDropdownClass', 'row'])--}}
 
-@if ($actionBuilder->hasView())
-    {!! $actionBuilder->getView($row) !!}
-@else
-    <div
-        class="flex items-center justify-center"
-        x-data="clickEvent"
-    >
-        @php
-            $hasName = $actionBuilder->hasName();
-            $disabled = $actionBuilder->isDisabled($row);
-            $hasDropdown = $actionBuilder->hasDropdown();
+{{--@if ($actionBuilder->hasView())--}}
+{{--    {!! $actionBuilder->getView($row) !!}--}}
+{{--@else--}}
+{{--    <div--}}
+{{--        class="flex items-center justify-center"--}}
+{{--        x-data="clickEvent"--}}
+{{--    >--}}
+{{--        @php--}}
+{{--            $hasName = $actionBuilder->hasName();--}}
+{{--            $disabled = $actionBuilder->isDisabled($row);--}}
+{{--            $hasDropdown = $actionBuilder->hasDropdown();--}}
 
-            $dropdownOptions = $actionBuilder->getActionOptions();
+{{--            $dropdownOptions = $actionBuilder->getActionOptions();--}}
 
-            $hasIcon = $actionBuilder->hasIcon();
+{{--            $hasIcon = $actionBuilder->hasIcon();--}}
 
-            $defaultOptionIcon = $actionBuilder->getDefaultOptionIcon();
+{{--            $defaultOptionIcon = $actionBuilder->getDefaultOptionIcon();--}}
 
-            $buttonStyle = $actionBuilder->getStyle();
+{{--            $buttonStyle = $actionBuilder->getStyle();--}}
 
-            $event = [
-                'event' => $actionBuilder->getEvent($row),
-                'hasDropdown' => $hasDropdown,
-                'disabled' => $disabled,
-            ];
+{{--            $event = [--}}
+{{--                'event' => $actionBuilder->getEvent($row),--}}
+{{--                'hasDropdown' => $hasDropdown,--}}
+{{--                'disabled' => $disabled,--}}
+{{--            ];--}}
 
-            $url = $actionBuilder->getUrl($row);
-            $target = $actionBuilder->getTarget();
-            $isWireNavigate = $actionBuilder->isWireNavigate();
-        @endphp
-        <x-simple-tables::dropdown
-            :$hasDropdown
-            :$dropdownOptions
-            :$defaultOptionIcon
-            :$themeDropdownOptionClass
-            :$themeDropdownClass
-            :$row
-        >
-            <x-slot:actionButton>
-                <a
-                    @if (filled($url)) href="{{ $url }}"
-                    target="{{ $target }}"
-                    @if ($isWireNavigate) wire:navigate @endif
-                @else
-                    x-on:click="handleClick('{{ json_encode($event) }}')"
-                    @endif
-                    x-ref="dropdownButton"
-                    @class([
-                        'gap-x-1.5' => $hasName,
-                        '!pointer-events-none !opacity-50' => $disabled,
-                        $themeButtonActionClass,
-                        $buttonStyle,
-                    ])>
-                    @if ($hasIcon)
-                        <x-dynamic-component
-                            :component="$actionBuilder->getIcon()"
-                            @class([
-                                '-mr-0.5' => $hasName,
-                                $actionBuilder->getIconStyle(),
-                            ])
-                        />
-                    @endif
-                    <span>{{ $actionBuilder->getName() }}</span>
-                </a>
-            </x-slot:actionButton>
-        </x-simple-tables::dropdown>
-    </div>
-@endif
+{{--            $url = $actionBuilder->getUrl($row);--}}
+{{--            $target = $actionBuilder->getTarget();--}}
+{{--            $isWireNavigate = $actionBuilder->isWireNavigate();--}}
+{{--        @endphp--}}
+{{--        <x-simple-tables::dropdown--}}
+{{--            :$hasDropdown--}}
+{{--            :$dropdownOptions--}}
+{{--            :$defaultOptionIcon--}}
+{{--            :$themeDropdownOptionClass--}}
+{{--            :$themeDropdownClass--}}
+{{--            :$row--}}
+{{--        >--}}
+{{--            <x-slot:actionButton>--}}
+{{--                <a--}}
+{{--                    @if (filled($url)) href="{{ $url }}"--}}
+{{--                    target="{{ $target }}"--}}
+{{--                    @if ($isWireNavigate) wire:navigate @endif--}}
+{{--                @else--}}
+{{--                    x-on:click="handleClick('{{ json_encode($event) }}')"--}}
+{{--                    @endif--}}
+{{--                    x-ref="dropdownButton"--}}
+{{--                    @class([--}}
+{{--                        'gap-x-1.5' => $hasName,--}}
+{{--                        '!pointer-events-none !opacity-50' => $disabled,--}}
+{{--                        $themeButtonActionClass,--}}
+{{--                        $buttonStyle,--}}
+{{--                    ])>--}}
+{{--                    @if ($hasIcon)--}}
+{{--                        <x-dynamic-component--}}
+{{--                            :component="$actionBuilder->getIcon()"--}}
+{{--                            @class([--}}
+{{--                                '-mr-0.5' => $hasName,--}}
+{{--                                $actionBuilder->getIconStyle(),--}}
+{{--                            ])--}}
+{{--                        />--}}
+{{--                    @endif--}}
+{{--                    <span>{{ $actionBuilder->getName() }}</span>--}}
+{{--                </a>--}}
+{{--            </x-slot:actionButton>--}}
+{{--        </x-simple-tables::dropdown>--}}
+{{--    </div>--}}
+{{--@endif--}}
---
+++ tests/Feature/BuilderDataset/Concerns/ActionBuilderTest.php
@@ -0,0 +1,267 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+
+use function Pest\Livewire\livewire;
+
+use TiagoSpem\SimpleTables\Action;
+use TiagoSpem\SimpleTables\Column;
+use TiagoSpem\SimpleTables\Concerns\ActionBuilder;
+use TiagoSpem\SimpleTables\Enum\Target;
+use TiagoSpem\SimpleTables\Facades\SimpleTables;
+use TiagoSpem\SimpleTables\SimpleTableComponent;
+use TiagoSpem\SimpleTables\Tests\Dummy\Model\FakeUser;
+use TiagoSpem\SimpleTables\Themes\DefaultTheme;
+
+beforeEach(function (): void {
+    FakeUser::factory(2)
+        ->state(new Sequence(
+            ['is_active' => true, 'name' => 'John Doe'],
+            ['is_active' => false, 'name' => 'Jane Doe'],
+        ))
+        ->create();
+});
+
+it('should be able to create an action builder as a simple button', function (): void {
+    $dynamicComponent = new class () extends SimpleTableComponent {
+        public function columns(): array
+        {
+            return [
+                Column::text('Name', 'name'),
+                Column::action('action', 'action column'),
+            ];
+        }
+
+        public function actionBuilder(): ActionBuilder
+        {
+            return SimpleTables::actionBuilder()
+                ->actions([
+                    Action::for('action')
+                        ->button(name: 'act button', href: 'https://example.com', wireNavigate: true, target: Target::BLANK),
+                ]);
+        }
+
+        public function datasource(): Builder
+        {
+            return FakeUser::query();
+        }
+    };
+
+    livewire($dynamicComponent::class)
+        ->assertSee(['act button', 'action column'])
+        ->assertSeeHtml('href="https://example.com"')
+        ->assertSeeHtml('target="_blank"')
+        ->assertSeeHtml('wire:navigate')
+        ->assertOk();
+});
+
+it('should be able to create an action button with a custom style', function (): void {
+    $dynamicComponent = new class () extends SimpleTableComponent {
+        public function columns(): array
+        {
+            return [
+                Column::text('Name', 'name'),
+                Column::action('action', 'action column'),
+            ];
+        }
+
+        public function actionBuilder(): ActionBuilder
+        {
+            return SimpleTables::actionBuilder()
+                ->actions([
+                    Action::for('action')
+                        ->button(name: 'act button')
+                        ->buttonStyle('button-with-custom-style'),
+                ]);
+        }
+
+        public function datasource(): Builder
+        {
+            return FakeUser::query();
+        }
+    };
+
+    $theme = (new DefaultTheme())->getStyles();
+
+    $themeButtonStyle = theme($theme, 'action.button');
+
+    $expectedButtonStyle = mergeStyle($themeButtonStyle, 'button-with-custom-style');
+
+    livewire($dynamicComponent::class)
+        ->assertSee(['act button', 'action column'])
+        ->assertSeeHtml($expectedButtonStyle)
+        ->assertOk();
+});
+
+it('should be able to disable action button', function (): void {
+    $dynamicComponent = new class () extends SimpleTableComponent {
+        public function columns(): array
+        {
+            return [
+                Column::text('Name', 'name'),
+                Column::action('action', 'action column'),
+            ];
+        }
+
+        public function actionBuilder(): ActionBuilder
+        {
+            return SimpleTables::actionBuilder()
+                ->actions([
+                    Action::for('action')
+                        ->button(name: 'act button', href: 'https://example.com', wireNavigate: true, target: Target::BLANK)
+                        ->disabled(),
+                ]);
+        }
+
+        public function datasource(): Builder
+        {
+            return FakeUser::query();
+        }
+    };
+
+    $theme = (new DefaultTheme())->getStyles();
+
+    $themeButtonStyle = theme($theme, 'action.button');
+
+    $expectedButtonStyle = mergeStyle('!pointer-events-none !opacity-50 disabled', $themeButtonStyle);
+
+    livewire($dynamicComponent::class)
+        ->assertSeeHtml($expectedButtonStyle)
+        ->assertDontSeeHtml('href="https://example.com"')
+        ->assertDontSeeHtml('wire:navigate')
+        ->assertDontSeeHtml('target="_blank"')
+        ->assertOk();
+});
+
+it('should be able to disable action button using callback', function (): void {
+    $dynamicComponent = new class () extends SimpleTableComponent {
+        public function columns(): array
+        {
+            return [
+                Column::text('Name', 'name')->searchable(),
+                Column::action('action', 'action column'),
+            ];
+        }
+
+        public function actionBuilder(): ActionBuilder
+        {
+            return SimpleTables::actionBuilder()
+                ->actions([
+                    Action::for('action')
+                        ->button(name: 'act button', href: 'https://example.com', wireNavigate: true, target: Target::BLANK)
+                        ->disabled(fn(FakeUser $user): bool => ! $user->is_active),
+                ]);
+        }
+
+        public function datasource(): Builder
+        {
+            return FakeUser::query();
+        }
+    };
+
+    $theme = (new DefaultTheme())->getStyles();
+
+    $themeButtonStyle = theme($theme, 'action.button');
+
+    $expectedButtonStyle = mergeStyle('!pointer-events-none !opacity-50 disabled', $themeButtonStyle);
+
+    livewire($dynamicComponent::class)
+        ->set('search', 'Jane Doe')
+        ->assertSeeHtml($expectedButtonStyle)
+        ->assertDontSeeHtml('href="https://example.com"')
+        ->assertDontSeeHtml('wire:navigate')
+        ->assertDontSeeHtml('target="_blank"')
+        ->set('search', 'John Doe')
+        ->assertSee('act button')
+        ->assertSeeHtml('href="https://example.com"')
+        ->assertSeeHtml('wire:navigate')
+        ->assertSeeHtml('target="_blank"')
+        ->assertSeeHtml('class="flex items-center justify-center"')
+        ->assertOk();
+});
+
+it('should be able to hidde action button', function (): void {
+    $dynamicComponent = new class () extends SimpleTableComponent {
+        public function columns(): array
+        {
+            return [
+                Column::text('Name', 'name'),
+                Column::action('action', 'action column'),
+            ];
+        }
+
+        public function actionBuilder(): ActionBuilder
+        {
+            return SimpleTables::actionBuilder()
+                ->actions([
+                    Action::for('action')
+                        ->button(name: 'act button', href: 'https://example.com', wireNavigate: true, target: Target::BLANK)
+                        ->hidden(),
+                ]);
+        }
+
+        public function datasource(): Builder
+        {
+            return FakeUser::query();
+        }
+    };
+
+    livewire($dynamicComponent::class)
+        ->assertDontSee('act button')
+        ->assertDontSeeHtml('href="https://example.com"')
+        ->assertDontSeeHtml('wire:navigate')
+        ->assertDontSeeHtml('target="_blank"')
+        ->assertDontSeeHtml('class="flex items-center justify-center"')
+        ->assertOk();
+});
+
+it('should be able to hidde action button using callback', function (): void {
+    $dynamicComponent = new class () extends SimpleTableComponent {
+        public function columns(): array
+        {
+            return [
+                Column::text('Name', 'name')->searchable(),
+                Column::action('action', 'action column'),
+            ];
+        }
+
+        public function actionBuilder(): ActionBuilder
+        {
+            return SimpleTables::actionBuilder()
+                ->actions([
+                    Action::for('action')
+                        ->button(name: 'act button', href: 'https://example.com', wireNavigate: true, target: Target::BLANK)
+                        ->hidden(fn(FakeUser $user): bool => ! $user->is_active),
+                ]);
+        }
+
+        public function datasource(): Builder
+        {
+            return FakeUser::query();
+        }
+    };
+
+    livewire($dynamicComponent::class)
+        ->set('search', 'Jane Doe')
+        ->assertDontSee('act button')
+        ->assertDontSeeHtml('href="https://example.com"')
+        ->assertDontSeeHtml('wire:navigate')
+        ->assertDontSeeHtml('target="_blank"')
+        ->assertDontSeeHtml('class="flex items-center justify-center"')
+        ->set('search', 'John Doe')
+        ->assertSee('act button')
+        ->assertSeeHtml('href="https://example.com"')
+        ->assertSeeHtml('wire:navigate')
+        ->assertSeeHtml('target="_blank"')
+        ->assertSeeHtml('class="flex items-center justify-center"')
+        ->assertOk();
+});
+
+it('create test for view button', function (): void {})->todo();
+
+it('create test for permission button', function (): void {})->todo();
+
+it('create test for dropdown button', function (): void {})->todo();
